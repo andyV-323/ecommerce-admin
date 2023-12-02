68baa3ddb48fa234f4d79cd8915d5caccc77d196
andyV-323 <40248914+andyV-323@users.noreply.github.com>
2023-12-01 19:31:51 -0800

    admin dashboard

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <Button size="default" variant="outline">
+        Click Me
+      </Button>
+    </div>
+  );
+}
